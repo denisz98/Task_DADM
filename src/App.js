2a6302d690df4d2901ac14e6ff8280bde10baba8
denisz98 <deniszaldivarperez98@gmail.com>
2023-10-17 11:59:09 -0400

    add: pageNotFound component to App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
     </BrowserRouter>
   )
